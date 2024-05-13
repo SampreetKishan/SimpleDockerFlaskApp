8dcb4688f19a60af2c974dd00277b02417da1656
Sampreet Kishan Kumar <sampy.prithvi2@gmail.com>
2024-05-13 13:49:11 -0400

    Second commit to the development branch

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #The app returns a simple message such as below
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask! Hurray'
 
 #The Flask web app runs on localhost and post 5000 on the container
 app.run(host='0.0.0.0', port=5000)
